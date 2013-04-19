03a2a757f3c0c9c7c6d10dbadd5f0ef2c921be97
Alonso Robles <alonso@alonsorobles.com>
2013-04-19 13:48:35 -0500

    Added code from session

---
+++ Tests/PlayingAround.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Linq;
+using System.Linq;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PlayingAround
+    {
+        private readonly IDocumentStore _documentStore;
+
+        public PlayingAround()
+        {
+            _documentStore = new DocumentStore
+                {
+                    Url = "http://localhost:8080",
+                    DefaultDatabase = "PlayingAround"
+                };
+            _documentStore.Initialize();
+        }
+
+
+        [Test]
+        public void CreateDocument()
+        {
+            MyDocument document;
+            using (var session = _documentStore.OpenSession())
+            {
+                document = new MyDocument
+                    {
+                        Content = "Some Content",
+                        Integer = 1,
+                        Double = 1.0,
+                        Decimal = 2m,
+                        Date = DateTime.UtcNow,
+                        Array = new[] {"Item 1", "Item 2"},
+                        ChildObject = new ChildObject {Hello = "World!"},
+                        AnonymousObject = new {Thing = "Stuff"}
+                    };
+                session.Store(document);
+                session.SaveChanges();
+            }
+            Assert.IsNotEmpty(document.Id);
+        }
+
+        [Test]
+        public void ModifyDocument()
+        {
+            const string modifiedContent = "Modified content";
+            var newDocument = new MyDocument
+                {
+                    Content = "New Content"
+                };
+            using (var session = _documentStore.OpenSession())
+            {
+                session.Store(newDocument);
+                session.SaveChanges();
+            }
+
+            using (var anotherSession = _documentStore.OpenSession())
+            {
+                var existingDocument = anotherSession.Load<MyDocument>(newDocument.Id);
+                existingDocument.Content = modifiedContent;
+                anotherSession.SaveChanges();
+            }
+
+            MyDocument modifiedDocument;
+            using (var yetAnotherSession = _documentStore.OpenSession())
+            {
+                modifiedDocument = yetAnotherSession.Load<MyDocument>(newDocument.Id);
+            }
+
+            Assert.AreEqual(modifiedDocument.Content, modifiedContent);
+            Assert.AreNotEqual(modifiedDocument.Content, newDocument.Content);
+            Assert.AreNotSame(modifiedDocument, newDocument);
+        }
+
+        [Test]
+        public void DeleteDocument()
+        {
+            var newDocument = new MyDocument
+                {
+                    Content = "New Content"
+                };
+            using (var session = _documentStore.OpenSession())
+            {
+                session.Store(newDocument);
+                session.SaveChanges();
+            }
+
+            using (var anotherSession = _documentStore.OpenSession())
+            {
+                var existingDocument = anotherSession.Load<MyDocument>(newDocument.Id);
+                anotherSession.Delete(existingDocument);
+                anotherSession.SaveChanges();
+            }
+
+            MyDocument deletedDocument;
+            using (var yetAnotherSession = _documentStore.OpenSession())
+            {
+                deletedDocument = yetAnotherSession.Load<MyDocument>(newDocument.Id);
+            }
+
+            Assert.IsNull(deletedDocument);
+        }
+
+        [Test]
+        public void QueryDocuments()
+        {
+            const string findableContent = "Finders keepers!";
+            const string otherContent = "Can't see me!";
+            var count = 0;
+            var newDocuments = new[]
+                {
+                    new MyDocument
+                        {
+                            Content = findableContent,
+                            Boolean = true,
+                            Integer = ++count,
+                            ChildObject = new ChildObject {Hello = "Query"}
+                        },
+                    new MyDocument
+                        {
+                            Content = findableContent,
+                            Boolean = true,
+                            Integer = ++count,
+                            ChildObject = new ChildObject {Hello = "Query"}
+                        },
+                    new MyDocument
+                        {
+                            Content = findableContent,
+                            Boolean = true,
+                            Integer = ++count,
+                            ChildObject = new ChildObject {Hello = "Query"}
+                        },
+                    new MyDocument
+                        {
+                            Content = findableContent,
+                            Boolean = true,
+                            Integer = ++count,
+                            ChildObject = new ChildObject {Hello = "Query"}
+                        },
+                    new MyDocument
+                        {
+                            Content = findableContent,
+                            Boolean = true,
+                            Integer = ++count,
+                            ChildObject = new ChildObject {Hello = "Query"}
+                        },
+                    new MyDocument
+                        {
+                            Content = otherContent,
+                            Boolean = false,
+                            Integer = ++count,
+                            ChildObject = new ChildObject {Hello = "Query"}
+                        },
+                    new MyDocument
+                        {
+                            Content = otherContent,
+                            Boolean = false,
+                            Integer = ++count,
+                            ChildObject = new ChildObject {Hello = "Query"}
+                        },
+                    new MyDocument
+                        {
+                            Content = otherContent,
+                            Boolean = false,
+                            Integer = ++count,
+                            ChildObject = new ChildObject {Hello = "Query"}
+                        },
+                    new MyDocument
+                        {
+                            Content = otherContent,
+                            Boolean = false,
+                            Integer = ++count,
+                            ChildObject = new ChildObject {Hello = "Query"}
+                        },
+                    new MyDocument
+                        {
+                            Content = otherContent,
+                            Boolean = false,
+                            Integer = ++count,
+                            ChildObject = new ChildObject {Hello = "Query"}
+                        }
+                };
+
+            using (var session = _documentStore.OpenSession())
+            {
+                foreach (var newDocument in newDocuments)
+                    session.Store(newDocument);
+                session.SaveChanges();
+            }
+
+            List<MyDocument> foundDocuments;
+            using (var anotherSession = _documentStore.OpenSession())
+            {
+                foundDocuments =
+                    anotherSession.Query<MyDocument>()
+                    .Where(doc => doc.Content == findableContent).ToList();
+            }
+            Assert.AreEqual(5, foundDocuments.Count);
+
+            using (var yetAnotherSession = _documentStore.OpenSession())
+            {
+                foundDocuments =
+                    yetAnotherSession.Query<MyDocument>()
+                    .Where(doc => doc.ChildObject.Hello == "Query").ToList();
+            }
+            Assert.AreEqual(10, foundDocuments.Count);
+
+            using (var cleanUpSession = _documentStore.OpenSession())
+            {
+                foreach (var newDocument in newDocuments)
+                {
+                    var deleteMe = cleanUpSession.Load<MyDocument>(newDocument.Id);
+                    cleanUpSession.Delete(deleteMe);
+                }
+                cleanUpSession.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Tests/SampleFixtureWithEmbeddableDocumentStore.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Embedded;
+
+namespace Tests
+{
+    [TestFixture]
+    public class SampleFixtureWithEmbeddableDocumentStore
+    {
+        private readonly IDocumentStore _embeddableDocumentStore;
+       
+        public SampleFixtureWithEmbeddableDocumentStore()
+        {
+            _embeddableDocumentStore = new EmbeddableDocumentStore().Initialize();
+        }
+
+        [Test]
+        public void SampleTest()
+        {
+            MyDocument document;
+            using (var session = _embeddableDocumentStore.OpenSession())
+            {
+                document = new MyDocument
+                    {
+                        Content = "Some Content",
+                        Integer = 1,
+                        Double = 1.0,
+                        Decimal = 2m,
+                        Date = DateTime.UtcNow,
+                        Array = new[] {"Item 1", "Item 2"},
+                        ChildObject = new ChildObject {Hello = "World!"},
+                        AnonymousObject = new {Thing = "Stuff"}
+                    };
+                session.Store(document);
+            }
+            Assert.IsNotEmpty(document.Id);
+        }
+    }
+
+    public class ChildObject
+    {
+        public string Hello { get; set; }
+    }
+
+    public class MyDocument
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public int Integer { get; set; }
+
+        public double Double { get; set; }
+
+        public decimal Decimal { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string[] Array { get; set; }
+
+        public ChildObject ChildObject { get; set; }
+
+        public object AnonymousObject { get; set; }
+
+        public object Boolean { get; set; }
+    }
+}
